3a51e83ce2316edae426fd3a927e8db2d0e05a56
Adam Kadwory <adamkadwory@gmail.com>
2025-07-22 22:53:23 -0700

    ep03

---
+++ phase_02_oop_stl/ep03_from-fucntion-to-object/ep03.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+using namespace std; 
+
+class Sensor{
+    private: 
+        float value; 
+
+    public:
+        void read(float newValue){
+            value = newValue; 
+        }
+
+        void calibrate(){
+            value = value - 0.5; 
+        }
+
+        void display(){
+            cout << "Sensor Value = " << value << endl;
+        }
+
+}; 
+
+int main(){
+    Sensor tempSensor;
+    tempSensor.read(78.3);
+    tempSensor.calibrate();
+    tempSensor.display();
+
+    Sensor gyroSensor; 
+    gyroSensor.read(0.045);
+    gyroSensor.calibrate();
+    gyroSensor.display(); 
+    
+
+    return 0; 
+}
